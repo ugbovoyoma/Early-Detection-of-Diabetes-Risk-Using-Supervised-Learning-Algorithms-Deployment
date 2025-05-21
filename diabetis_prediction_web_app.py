2cc3cda1418481e693525cabb85e5b07147229b1
Ugbovo Yoma <155391259+ugbovoyoma@users.noreply.github.com>
2025-05-21 12:16:26 +0100

    Update diabetis_prediction_web_app.py

--- diabetis_prediction_web_app.py
+++ diabetis_prediction_web_app.py
@@ -12,7 +12,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model = pickle.load(open(r'c:\Users\CB\Desktop\Diabetics_Prediction\trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 loaded_model
 
